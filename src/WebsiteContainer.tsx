8dd9ca8187b3928f42c470194d6674f12748d674
wimblewamble <s3784709@student.rmit.edu.au>
2022-11-10 18:02:43 +1100

    Add header styling

--- src/WebsiteContainer.tsx
+++ src/WebsiteContainer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 import { IconSun, IconMoonStars, IconPrompt } from '@tabler/icons';
-import { AppShell, Header, Group, ActionIcon, useMantineColorScheme } from '@mantine/core';
+import { AppShell, Header, Group, ActionIcon, useMantineColorScheme, UnstyledButton, Title, Button, Space } from '@mantine/core';
 
 export default function WebsiteContainer() {
     const { colorScheme, toggleColorScheme } = useMantineColorScheme();
@@ -15,10 +15,29 @@ export default function WebsiteContainer() {
           header={
             <Header height={60}>
               <Group sx={{ height: '100%' }} px={20} position="apart">
-                <IconPrompt/>
+                
+                <Group>
+                  <UnstyledButton onClick={() => {}}>
+                    <Group position='center'>
+                      <IconPrompt/>
+                      <Title size={24}>Matthew Soulsby</Title>
+                    </Group>
+                  </UnstyledButton>
+                  
+                  <Space w='md'/>
+                  
+                  <Button variant="subtle">
+                    Home
+                  </Button>
+                  <Button variant="subtle">
+                    Projects
+                  </Button>
+                </Group>
+                
                 <ActionIcon variant="default" onClick={() => toggleColorScheme()} size={30}>
                   {colorScheme === 'dark' ? <IconSun size={16} /> : <IconMoonStars size={16} />}
                 </ActionIcon>
+
               </Group>
             </Header>
           }
